72c775e63dd3863f569a672aadafab2e2e6cb5a0
kyle-shk <guqtls85@naver.com>
2022-10-05 01:30:52 +0900

    초기화

--- src/App.jsx
+++ src/App.jsx
@@ -1,20 +1,5 @@
-import FilterableProductTable from "./components/FilterableProductTable";
-
-const PRODUCTS = [
-  { category: "Fruits", price: "$1", stocked: true, name: "Apple" },
-  { category: "Fruits", price: "$1", stocked: true, name: "Dragonfruit" },
-  { category: "Fruits", price: "$2", stocked: false, name: "Passionfruit" },
-  { category: "Vegetables", price: "$2", stocked: true, name: "Spinach" },
-  { category: "Vegetables", price: "$4", stocked: false, name: "Pumpkin" },
-  { category: "Vegetables", price: "$1", stocked: true, name: "Peas" },
-];
-
 function App() {
-  return (
-    <div className="App">
-      <FilterableProductTable products={PRODUCTS} />
-    </div>
-  );
+  return <div className="App"></div>;
 }
 
 export default App;
