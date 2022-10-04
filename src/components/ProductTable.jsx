df138a6daac22ff5157f3ba59070b1808b5e1584
kyle-shk <guqtls85@naver.com>
2022-10-05 00:34:16 +0900

    세팅 및 check & event props

---
+++ src/components/ProductTable.jsx
@@ -0,0 +1,60 @@
+import ProductRow from "./ProductRow";
+import ProductCategoryRow from "./ProductCategoryRow";
+
+export default function ProductTable({ products, filterText, inStockOnly }) {
+  // forEach이용
+  //   const rows = [];
+  //   let change = null;
+  //   products.forEach((product) => {
+  //     if (product.category !== change) {
+  //       rows.push(
+  //         <ProductCategoryRow
+  //           category={product.category}
+  //           key={product.category}
+  //         />
+  //       );
+  //     }
+  //     rows.push(<ProductRow product={product} key={product.name} />);
+  //     change = product.category;
+  //   });
+
+  //   map이용
+  const rows = products.map((product, idx) => {
+    console.log(idx);
+    if (idx == 0) {
+      return (
+        <>
+          <ProductCategoryRow
+            category={product.category}
+            key={product.category}
+          />
+          <ProductRow product={product} key={product.name} />
+        </>
+      );
+    } else if (product.category === products[idx - 1].category) {
+      return <ProductRow product={product} key={product.name} />;
+    } else {
+      return (
+        <>
+          <ProductCategoryRow
+            category={product.category}
+            key={product.category}
+          />
+          <ProductRow product={product} key={product.name} />
+        </>
+      );
+    }
+  });
+
+  return (
+    <table>
+      <thead>
+        <tr>
+          <th>Name</th>
+          <th>Price</th>
+        </tr>
+      </thead>
+      <tbody>{rows}</tbody>
+    </table>
+  );
+}
