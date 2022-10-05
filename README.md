4f5d644bfd6c75a18b0fc926b2526fb6560c3519
kyle-shk <guqtls85@naver.com>
2022-10-05 22:53:04 +0900

    rendering lists 과제3

--- README.md
+++ README.md
@@ -1,14 +1,17 @@
 # ReactTutorial-beta
 
 ## todolist
+
 ### Quick start
-   - [x] Thinking in React(filtering list & check input)
-   
+
+- [x] Thinking in React(filtering list & check input)
+
 ### Describing the UI
-   - [] Your First Component
-   - [] importing and Exporting Components
-   - [] Writing Markup with Jsx
-   - [] Js in jsx with Curly Braces
-   - [] Passing Props to a Component
-   - [] Conditional Rendering
-   - [] Rendering Lists
+
+- [x] Your First Component
+- [x] importing and Exporting Components
+- [x] Writing Markup with Jsx
+- [] Js in jsx with Curly Braces
+- [] Passing Props to a Component
+- [] Conditional Rendering
+- [] Rendering Lists
--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,51 @@
-function App() {
-  return <div className="App"></div>;
-}
+// Rendering Lists
+
+import { recipes } from "./data.js";
+
+// export default function App() {
+//   return (
+//     <div>
+//       <h1>Recipes</h1>
+//       {recipes.map((recipe) => {
+//         const { id, name, ingredients } = recipe;
+//         return (
+//           <div key={id}>
+//             <h2>{name}</h2>
+//             <ul>
+//               {ingredients.map((ingredient) => (
+//                 <li key={ingredient}>{ingredient}</li>
+//               ))}
+//             </ul>
+//           </div>
+//         );
+//       })}
+//     </div>
+//   );
+// }
+
+const Recipe = ({ id, name, ingredients }) => {
+  return (
+    <div>
+      <h2>{name}</h2>
+      <ul>
+        {ingredients.map((ingredient) => {
+          return <li key={ingredient}>{ingredient}</li>;
+        })}
+      </ul>
+    </div>
+  );
+};
+
+// 부모 -> 자식
+const App = () => {
+  return (
+    <div>
+      <h1>Recipes</h1>
+      {recipes.map((recipe) => {
+        return <Recipe {...recipe} key={recipe.id} />;
+      })}
+    </div>
+  );
+};
 
 export default App;
