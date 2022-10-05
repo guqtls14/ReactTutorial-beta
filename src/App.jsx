42454805435958cf58e71b55c5f4b12420a1f6b5
kyle-shk <guqtls85@naver.com>
2022-10-05 23:14:22 +0900

    rendering lists 4

--- src/App.jsx
+++ src/App.jsx
@@ -1,51 +1,75 @@
-// Rendering Lists
+// // Rendering Lists
 
-import { recipes } from "./data.js";
+// import { recipes } from "./data.js";
 
-// export default function App() {
+// // export default function App() {
+// //   return (
+// //     <div>
+// //       <h1>Recipes</h1>
+// //       {recipes.map((recipe) => {
+// //         const { id, name, ingredients } = recipe;
+// //         return (
+// //           <div key={id}>
+// //             <h2>{name}</h2>
+// //             <ul>
+// //               {ingredients.map((ingredient) => (
+// //                 <li key={ingredient}>{ingredient}</li>
+// //               ))}
+// //             </ul>
+// //           </div>
+// //         );
+// //       })}
+// //     </div>
+// //   );
+// // }
+
+// const Recipe = ({ id, name, ingredients }) => {
+//   return (
+//     <div>
+//       <h2>{name}</h2>
+//       <ul>
+//         {ingredients.map((ingredient) => {
+//           return <li key={ingredient}>{ingredient}</li>;
+//         })}
+//       </ul>
+//     </div>
+//   );
+// };
+
+// // 부모 -> 자식
+// const App = () => {
 //   return (
 //     <div>
 //       <h1>Recipes</h1>
 //       {recipes.map((recipe) => {
-//         const { id, name, ingredients } = recipe;
-//         return (
-//           <div key={id}>
-//             <h2>{name}</h2>
-//             <ul>
-//               {ingredients.map((ingredient) => (
-//                 <li key={ingredient}>{ingredient}</li>
-//               ))}
-//             </ul>
-//           </div>
-//         );
+//         return <Recipe {...recipe} key={recipe.id} />;
 //       })}
 //     </div>
 //   );
-// }
+// };
 
-const Recipe = ({ id, name, ingredients }) => {
-  return (
-    <div>
-      <h2>{name}</h2>
-      <ul>
-        {ingredients.map((ingredient) => {
-          return <li key={ingredient}>{ingredient}</li>;
-        })}
-      </ul>
-    </div>
-  );
+// export default App;
+
+// Rendering Lists 4
+import React, { Fragment } from "react";
+
+const poem = {
+  lines: [
+    "I write, erase, rewrite",
+    "Erase again, and then",
+    "A poppy blooms.",
+  ],
 };
 
-// 부모 -> 자식
-const App = () => {
+export default function Poem() {
   return (
-    <div>
-      <h1>Recipes</h1>
-      {recipes.map((recipe) => {
-        return <Recipe {...recipe} key={recipe.id} />;
-      })}
-    </div>
+    <article>
+      {poem.lines.map((line, i) => (
+        <Fragment key={i}>
+          {i > 0 && <hr />}
+          <p>{line}</p>
+        </Fragment>
+      ))}
+    </article>
   );
-};
-
-export default App;
+}
